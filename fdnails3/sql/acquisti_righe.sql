7d7cfc1ff82a64a5d1e307f0767a63c797e1aefb
francescadonofrio <francydono02@gmail.com>
2026-03-18 14:17:49 +0100

    Delete fdnails3 directory

--- fdnails3/sql/acquisti_righe.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS acquisti_righe (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  id_acquisto INT NOT NULL,
-  id_smalto VARCHAR(8) NOT NULL,
-  quantita INT UNSIGNED NOT NULL DEFAULT 1,
-  prezzo_unitario DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-  FOREIGN KEY (id_acquisto) REFERENCES acquisti(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
